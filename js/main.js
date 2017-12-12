7c9876c9c24951f51a34be063f054a676d078843
Nikola Dosenovic <nikoladosenovic92@gmail.com>
2017-12-12 14:49:51 +0100

    podesavanje countera

--- js/main.js
+++ js/main.js
@@ -1,34 +1,77 @@
-$(document).ready(function(){
-    
-   $('.navbar-nav a').click(function () {
+$(document).ready(function () {
+
+    $('.navbar-nav a, .landing .container > a').click(function () {
 
         $('html, body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top
         }, 2000);
 
-    }); 
-    
+    });
+
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
         if (scroll > 150) {
             $('header').addClass('py-2');
             $('.navbar-brand').addClass('small-logo');
-            $('header').css('background-color','rgba(0,0,0,0.8)');
-            
+            $('header').css('background-color', 'rgba(0,0,0,0.8)');
+
         } else {
             $('header').removeClass('py-2');
-             $('.navbar-brand').removeClass()('small-logo');
-             $('header').css('background-color','rgba(0,0,0,0.3)');
+            $('.navbar-brand').removeClass('small-logo');
+            $('header').css('background-color', 'rgba(0,0,0,0.3)');
         }
     });
+
+
+
+// round slider for % 
+
+    $(window).scroll(function () {
+        
+        precent();
+
+        
+
+    });
+     precent();
     
-    
-    
-    
-    
-    
-    
-    
+    function precent (){
+        var vh = $(window).height();
+        var scroll = $(window).scrollTop();
+
+        $('.cont').each(function () {
+            var position = $(this).offset().top;
+            
+            if(position < vh + scroll - 100){
+                
+            var val = parseInt($(this).attr('data-pct'));
+            var $circle = $(this).find('.svg .bar');
+
+           
+                var r = $circle.attr('r');
+                var c = Math.PI * (r * 2);
+
+                
+
+                var pct = ((100 - val) / 100) * c;
+
+                $circle.css({strokeDashoffset: pct});
+
+            }
+
+
+
+
+                //$('#cont').attr('data-pct', val);
+            
+        });
+    }
+
+
+
+
+
+
 });
 
 
